f7625c03eed499c152f6c143217f61fedd991bd7
rohithmarcus <rohithmarcus@porto.la>
2018-01-31 18:59:48 +0530

    Initial Commits

---
+++ AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenterSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            AppCenter.Start("android=5d45dc8b-49fa-4981-9df1-d59773e7e932;" +
+                   "ios=f0c4e99c-9385-4596-8c8d-400889c7c4e6;",
+                   typeof(Analytics), typeof(Crashes));
+
+            MainPage = new AppCenterSample.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
